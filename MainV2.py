20b313d40a47835e9e8694eb97fd44363f875f27
Bartbabart <Bart.de.vries@outlook.com>
2021-03-09 15:00:16 +0100

    wachttijd voor lift

--- MainV2.py
+++ MainV2.py
@@ -6,7 +6,7 @@ def main(tijd):
     env = simpy.Environment()
     Ele = Elevator(env, 1)
     levellist = list(Ele.level_dictionary.keys())
-    Hum1 = Human(levellist,env, Ele)
+    Hum1 = Human(levellist, env, Ele)
     env.run(until=tijd)
     print(f"Simulation gestopt op: {tijd}")
 
@@ -32,22 +32,20 @@ class Elevator(object):
 
     def find_duration_moving(self):
         current = self.level_dictionary[self.level]
+        print(self.destination)
         destination = self.level_dictionary[self.destination]
-        if current > destination:
-            return (current - destination) / self.speed
-        return (destination - current) / self.speed
+        return (abs(destination - current)) / self.speed
 
     def set_destinations(self):
-        # For every human
-        # If any human in inside state:
-        # Get wanted_list from all humans
-        # Go to closest from this list
-        # If elevator meets another current_etage:
-        # Go there first
-        # Else
-        # Get current_etages from all humans
-        # Go to closest from this list
-        return random.randint(0, 4)
+        if not self.destination_list:
+            return None
+        else:
+            shortest = 100
+            for i in self.destination_list:
+                diff = abs((self.level - i))
+                if diff < shortest:
+                    shortest = diff
+            return shortest
 
     def level_dict(self, height, level_amount):
         # Maakt een dictionary van elke etage en gebruikt de gemiddelde afstand tussen de etages)
@@ -67,32 +65,35 @@ class Elevator(object):
 
     def run(self):
         while True:
-            print(f'Elevator starts moving at %d moving from {self.level} to {self.destination}' % self.env.now)
-            move_duration = self.find_duration_moving()
-            self.level = self.destination
-            self.destination = self.set_destinations()
-            yield self.env.process(self.charge(move_duration))
+            if self.destination_list:
+                print(f'Elevator starts moving at %d moving from {self.level} to {self.destination}' % self.env.now)
+                move_duration = self.find_duration_moving()
+                self.level = self.destination
+                self.destination = self.set_destinations()
+                yield self.env.process(self.charge(move_duration))
 
-            print('Elevator starts opening at %d' % self.env.now)
-            door_duration = 2
-            yield self.env.timeout(door_duration)
+                print('Elevator starts opening at %d' % self.env.now)
+                door_duration = 2
+                yield self.env.timeout(door_duration)
 
-            print(f'Elevator is open at %d' % self.env.now)
-            self.state = 1
-            open_duration, total_open_duration = 10, 10
-            while open_duration < 0:
-                if open_duration < 3 and self.sensor == True:
-                    total_open_duration += 5 - open_duration
-                    open_duration = 5
-                    self.sensor = False
-                total_open_duration += 5
-            yield self.env.process(self.charge(total_open_duration))
+                print(f'Elevator is open at %d' % self.env.now)
+                self.state = 1
+                open_duration, total_open_duration = 10, 10
+                while open_duration < 0:
+                    if open_duration < 3 and self.sensor == True:
+                        total_open_duration += 5 - open_duration
+                        open_duration = 5
+                        self.sensor = False
+                    total_open_duration += 5
+                yield self.env.process(self.charge(total_open_duration))
 
-            print('Elevator starts closing at %d' % self.env.now)
-            door_duration = 2
-            yield self.env.timeout(door_duration)
-            self.state = 0
-            yield self.env.timeout(1)
+                print('Elevator starts closing at %d' % self.env.now)
+                door_duration = 2
+                yield self.env.timeout(door_duration)
+                self.state = 0
+                yield self.env.timeout(1)
+            else:
+                yield self.env.process(self.charge(1))
 
     def charge(self, duration):
         yield self.env.timeout(duration)
@@ -151,9 +152,8 @@ class Human(object):
             print(
                 f'Human presses elevator button at %d from level {self.level}.' % self.env.now)  # mens staat bij lift deur.
             self.other.destination_list.append(self.level)
-            """ 
-                add level to destinations
-            """
+            self.give_current(self.level)
+
             while True:
 
                 try:
@@ -164,9 +164,7 @@ class Human(object):
 
                         print(f'human is in lift at %d' % self.env.now)
 
-                        """ 
-                        add eind_etage to destinations
-                        """
+                        self.give_destination(self.destination)
 
                         while True:
                             if self.other.state and self.other.level == "TODO" and self.state == 1:  # lift is op eind_etage.
@@ -187,6 +185,16 @@ class Human(object):
                     self.lives -= 1
                     pass
 
+    def give_current(self, level):
+        if level not in self.other.destination_list:
+            self.other.destination_list.append(level)
+        return None
+
+    def give_destination(self, dest_level):
+        if dest_level not in self.other.destination_list:
+            self.other.destination_list.append(dest_level)
+        return None
+
     def wachttijd(self, duration):
         yield self.env.timeout(duration)
 
@@ -204,3 +212,6 @@ class Human(object):
     def get_destination(self):
         return None
 
+
+if __name__ == '__main__':
+    main(500)
